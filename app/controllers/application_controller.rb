eebfb146e4448ac66a20fde460e2b21324df8e12
ljleppan <leo.leppanen@helsinki.fi>
2014-05-01 23:14:45 +0300

    Cleanup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :current_user, :logged_in?, :ensure_login, :ensure_admin
-
+  helper_method :current_user, :logged_in?, :ensure_login
   def current_user
     return nil if session[:user_id].nil?
     User.find(session[:user_id])
